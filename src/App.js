4e911ffcefbc28246b6d8fe5d81bad498d29abf7
AndyIsCool5463 <andyl5463@gmail.com>
2020-07-29 20:37:04 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const getData = async () => {
+    let data = await fetch("/bonk", { method: "POST" });
+    console.log(data);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={getData}>hi</button>
         <a
           className="App-link"
           href="https://reactjs.org"
